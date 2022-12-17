d44347aaed5a7bd50563ef40247ede8091dcba9c
Levent Eren <levent.eren@deop.ca>
2022-12-17 13:51:48 -0500

    new changes

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "rg_kurs" {
 resource "azurerm_storage_account" "storage_kurs" {
   name                     = "tfcloudstorage0099880"
   resource_group_name      = azurerm_resource_group.rg_kurs.name
-  location                 = azurerm_resource_group.storage_kurs.location
+  location                 = azurerm_resource_group.rg_kurs.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
